8a0aff0fa936a8ada2677b1828041a719a31a913
fadayo2 <afadunmiye@gmail.com>
2024-05-06 13:06:08 -0700

    add javascript documents to the project

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-const nightmode = () => {
-    document.querySelector("body").style.backgroundColor = 'rgb(8, 2, 34';
-    document.querySelector("body").style.color = 'white';
-    document.getElementById('nightmode').style.display = 'none';
-    document.getElementById('lightmode').style.display = 'flex';
-}
+// const nightmode = () => {
+//     document.querySelector("body").style.backgroundColor = 'rgb(8, 2, 34';
+//     document.querySelector("body").style.color = 'white';
+//     document.getElementById('nightmode').style.display = 'none';
+//     document.getElementById('lightmode').style.display = 'flex';
+// }
 
-const lightmode = () => {
-    document.querySelector("body").style.color = 'black';
-    document.querySelector('body').style.backgroundColor = 'white';
-    document.getElementById('nightmode').style.display = 'flex';
-    document.getElementById('lightmode').style.display = 'none';
-}
+// const lightmode = () => {
+//     document.querySelector("body").style.color = 'black';
+//     document.querySelector('body').style.backgroundColor = 'white';
+//     document.getElementById('nightmode').style.display = 'flex';
+//     document.getElementById('lightmode').style.display = 'none';
+// }
 
 const html = () =>{
     window.location.href = 'HTML.html'
@@ -18,6 +18,10 @@ const html = () =>{
 
 const css = () =>{
     window.location.href = 'CSS.html'
+}
+
+const js = () =>{
+    window.location.href = 'js.html'
 }
 
 const changemode = () => {
